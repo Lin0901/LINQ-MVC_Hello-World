d431f1584c63dd46d2ea25587642cd224903d020
Lin <944799613@qq.com>
2022-07-10 12:53:02 -0500

    first commit

---
+++ HelloWorld/Controllers/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class Calculator : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string calculator()
+        {   
+            int a = 0;
+            int b = 0;
+
+            string add = "+";
+            string subtract = "-";
+            string multiply = "*";
+            string divide = "/";
+
+            return calculator();
+        }
+    }
+}
---
+++ HelloWorld/Controllers/HelloWorld.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SayHello(string age)
+        {
+            string name = "Lin";
+            string customMessage = $"Welcome, {name}. Your age is {age}";
+
+            return View();
+        }
+
+        public IActionResult Calculator(int firstOperant, int secondOperant, string operation)
+        {
+            int result = 0;
+            string verb = "";
+
+            try 
+            {
+                switch (operation)
+                {
+                    case "add":
+                        {
+                            result = firstOperant + secondOperant;
+                            verb = "plus";
+                            break;
+                        }
+                    case "subtract":
+                        {
+                            result = firstOperant - secondOperant;
+                            verb = "minus";
+                            break;
+                        }
+                    case "multiply":
+                        {
+                            result = firstOperant * secondOperant;
+                            verb = "multiply by";
+                            break;
+                        }
+                    case "divide":
+                        {
+                            result = firstOperant / secondOperant;
+                            verb = "divided by";
+                            break;
+                        }
+                };
+
+                ViewData["result"] = result;
+                ViewData["verb"] = verb;
+                ViewData["firstNumber"] = firstOperant;
+                ViewData["secondNumber"] = secondOperant;
+
+                return View();
+
+            } catch(Exception ex)
+            {
+                return View("Error");
+            }
+
+            
+
+            
+        }
+        
+    }
+}
